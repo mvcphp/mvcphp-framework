abf53c992b7e7eb4485d446e45b88c434b39c156
mvcphp <mvcphp@mail.ru>
2013-10-22 19:45:20 +0400

    mvcphp framework site Reformal

--- application/views/themes/default/ru/template_main.php
+++ application/views/themes/default/ru/template_main.php
@@ -51,5 +51,28 @@
         <noscript><div><img src="//mc.yandex.ru/watch/22201663" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
         <!-- /Yandex.Metrika counter -->
         
+        <!-- Reformal -->
+        <script type="text/javascript">
+            var reformalOptions = {
+                project_id: 213994,
+                project_host: "mvcphp.reformal.ru",
+                tab_orientation: "left",
+                tab_indent: "50%",
+                tab_bg_color: "#278f33",
+                tab_border_color: "#FFFFFF",
+                tab_image_url: "http://tab.reformal.ru/T9GC0LfRi9Cy0Ysg0Lgg0L%252FRgNC10LTQu9C%252B0LbQtdC90LjRjw==/FFFFFF/2a94cfe6511106e7a48d0af3904e3090/left/1/tab.png",
+                tab_border_width: 2
+            };
+
+            (function() {
+                var script = document.createElement('script');
+                script.type = 'text/javascript'; script.async = true;
+                script.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'media.reformal.ru/widgets/v3/reformal.js';
+                document.getElementsByTagName('head')[0].appendChild(script);
+            })();
+        </script>
+        <noscript><a rel="nofollow" href="http://mvcphp.reformal.ru" target="_blank">Oтзывы и предложения для MVCPHP Framework</a></noscript>
+        <!-- /Reformal -->
+        
     </body>
 </html>
